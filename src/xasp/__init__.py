c32131aa1e3f5d66c7b007eeb57d08a6fb21e14e
enosama2006 <43531597+enosama2006@users.noreply.github.com>
2026-07-22 21:43:21 +0300

    feat: initialize XASP Python package

---
+++ src/xasp/__init__.py
@@ -0,0 +1,5 @@
+"""XASP: governed first-touch shock prediction research toolkit."""
+
+from .labeling import BarrierConfig, BarrierLabel, label_first_touch
+
+__all__ = ["BarrierConfig", "BarrierLabel", "label_first_touch"]
